a3f9e4c642b708ce278f40cca150eba230806b9b
Yerkebulan Aben <yerkebulan.aben@gmail.com>
2021-05-04 15:38:01 +0600

    Create todo-list component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import StatusFilter from '../status-filters';
+import TodoList from '../todo-list';
 import './app.css';
 
 export default class App extends Component {
@@ -18,6 +19,7 @@ export default class App extends Component {
         <AppHeader />
         <SearchPanel />
         <StatusFilter />
+        <TodoList />
       </div>
     );
   }
---
+++ src/components/todo-list/todo-list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TodoListItem from '../todo-list-item';
+import './todo-list.css';
+
+const TodoList = () => {
+    return(
+        <TodoListItem />
+    );
+}
+
+export default TodoList;
