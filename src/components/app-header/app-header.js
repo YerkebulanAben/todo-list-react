8a2436e0a554daa03c31f8e991666b6f6792b98c
Yerkebulan Aben <yerkebulan.aben@gmail.com>
2021-05-04 14:59:09 +0600

    Create app-header component

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app-header.css';
+
+const AppHeader = () => {
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <h2>3 more to do, 1 done</h2>
+    </div>
+    );
+}
+
+export default AppHeader;
