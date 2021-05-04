40922c440f5e5552c74dd1d6998d3f3e56d8542a
Yerkebulan Aben <yerkebulan.aben@gmail.com>
2021-05-04 15:21:08 +0600

    Create search-panel component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
 import './app.css';
 
 export default class App extends Component {
   render() {
     return(
-      <AppHeader />
+      <div>
+        <AppHeader />
+        <SearchPanel /> 
+      </div>
     );
   }
 }
